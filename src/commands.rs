00ddf9e4ae0478c2e91505bf310f7a59f0a37018
Jannis <jannis2401p@gmail.com>
2026-06-24 15:10:33 +0200

    neu

--- src/commands.rs
+++ src/commands.rs
@@ -1,16 +1,18 @@
 use std::process::Command;
 
-pub fn handle(input: &String) -> String {
+pub fn handle(input: &str) -> String {
+    #[cfg(target_os = "windows")]
+    let output = Command::new("cmd").args(&["/C", input]).output();
+
+    #[cfg(not(target_os = "windows"))] // Linux, macOS, etc.
     let output = Command::new("sh").arg("-c").arg(input).output();
 
     match output {
         Ok(output) => {
             let stdout = String::from_utf8_lossy(&output.stdout);
             let stderr = String::from_utf8_lossy(&output.stderr);
-
-            // Beide zusammengeben
             format!("{}{}", stdout, stderr)
         }
-        Err(e) => e.to_string(), // Fehler beim Starten des Befehls
+        Err(e) => e.to_string(),
     }
 }
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,7 @@
 pub use directories::ProjectDirs;
 pub use eframe::egui;
 pub use std::path::PathBuf;
+pub use std::process::{Command, Stdio};
+pub use std::sync::mpsc::{channel, Receiver, Sender};
+pub use std::thread;
 pub use std::time::Instant;
